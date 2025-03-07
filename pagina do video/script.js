f533c08860009424e3239e96a0e32d6d6d4cfee4
zerodavi <crf2233x@gmail.com>
2025-03-07 11:18:43 -0300

    Delete pagina do video directory

--- pagina do video/script.js
+++
@@ -1,7 +0,0 @@
-// Função para ativar o menu lateral
-const menuToggle = document.querySelector('.menu-toggle');
-const sideMenu = document.querySelector('.side-menu');
-
-menuToggle.addEventListener('click', function() {
-    sideMenu.classList.toggle('closed'); // Alterna a visibilidade do menu lateral
-});
